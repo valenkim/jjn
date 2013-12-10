4e8ee4ac0aa0f03a8edf8438123a928daf36a20c
Kwak <9-4-2-8@hanmail.net>
2013-12-11 00:49:04 +0900

    +CompanyInfo

--- WebContent/SelectMealView.jsp
+++ WebContent/SelectMealView.jsp
@@ -5,9 +5,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>음식점 선택하기</title>
-
 </head>
 <body>
+<form action="MealManager" method="get">
+<%
+double lng = Double.valueOf(request.getAttribute("lng").toString());
+double  lat =Double.valueOf( request.getAttribute("lat").toString());
+%>
 
+좌표 <%=lng%>, <%=lat %>
+</form>
 </body>
 </html>
--- src/manager/MealManager.java
+++ src/manager/MealManager.java
@@ -1,10 +1,17 @@
 package manager;
 
+import gson.GsonView;
+
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Info.Item;
 
 
 public class MealManager extends HttpServlet {
@@ -12,6 +19,25 @@ public class MealManager extends HttpServlet {
        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		GsonView gv = new GsonView();
+		
+		String add = request.getParameter("add");
+		Item i;
+		
+		i = gv.changeAddress(add);
+
+		
+		gv.distance(i.lat, i.lng, j.lat, j.lng);
+		System.out.println(i.lng);
+		request.setAttribute("lng",i.lng);
+		request.setAttribute("lat",i.lat);
+		request.setAttribute("title",i.title);
+		
+		
+		ServletContext context = request.getSession().getServletContext();
+		RequestDispatcher rd  = context.getRequestDispatcher("/SelectMealView.jsp");
+		rd.forward(request, response);		
 		
 	}
 
